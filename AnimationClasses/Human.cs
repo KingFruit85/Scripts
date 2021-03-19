a52713780d310f14130cf50a50ccc348a2cce014
KingFruit85 <christopher.aaron.long@gmail.com>
2021-03-19 12:24:42 +0000

    inital commit

--- AnimationClasses/Human.cs
+++ AnimationClasses/Human.cs
@@ -15,11 +15,8 @@ public class Human : MonoBehaviour
     private string knockRight;
     private string knockUp;
     private string knockDown;
-
     public int swordDamage = 10;
     public float swordRange = 0.5f;
-
-
 
     public Human()
     {
@@ -31,13 +28,70 @@ public class Human : MonoBehaviour
         walkDown = "Human_Move_Down";
         idleUp = "Human_Idle_Up";
         idleDown = "Human_Idle_Down";
-        death = "Human_Death";
-
+        // death = "Human_Death";
         knockLeft = "Human_KnockArrow_Left";
         knockRight = "Human_KnockArrow_Right";
+    }
 
-        
-        
+    public float dashSpeed = 80f;
+    public float dashCoolDown = -9999;
+    public float dashDelay = 2f;
+    private bool canDash;
+    private bool isDashing;
+
+    public void Dash()
+    {
+        if (canDash)
+        {
+            canDash = false;
+            StartCoroutine(ToggleIsDashingBool());
+
+            switch (direction)
+            {   
+                default: throw new System.Exception("invalid dash direction provided");
+                case Looking.Up:
+                                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + dashSpeed); 
+                                anim.Play("Human_Dash_Up");
+                                shaker.CombatShaker("Up");
+                                dashCoolDown = Time.time;
+                                break;
+
+                case Looking.Down:
+                                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y -dashSpeed); 
+                                anim.Play("Human_Dash_Down");
+                                shaker.CombatShaker("Down");
+                                dashCoolDown = Time.time;
+                                break;
+
+                case Looking.Left:
+                                rb.velocity = new Vector2(rb.velocity.x - dashSpeed, rb.velocity.y);
+                                anim.Play("Human_Dash_Left");
+                                dashCoolDown = Time.time;
+                                break;
+
+                case Looking.Right:
+                                rb.velocity = new Vector2(rb.velocity.x + dashSpeed, rb.velocity.y);
+                                anim.Play("Human_Dash_Right");
+                                shaker.CombatShaker("Right");
+                                dashCoolDown = Time.time;
+                                break;
+            }
+        }
+
+    }
+
+    private IEnumerator ToggleIsDashingBool()
+    {
+        isDashing = true;
+        // rb.isKinematic = true;
+        yield return new WaitForSeconds( 0.3f );
+        // rb.isKinematic = false;
+        isDashing = false;
+    }
+
+    public bool isPlayerDashing()
+    {
+        return isDashing;
     }
 
     public void SwordAttack(Vector2 attackPoint, LayerMask enemyLayers)
@@ -68,6 +122,15 @@ public class Human : MonoBehaviour
 
         }
 
+    }
+
+    void Update()
+    {
+        if (Time.time > dashCoolDown + dashDelay)
+        {
+            canDash = true;
+            dashCoolDown = Time.time;
+        }
     }
 
 }
